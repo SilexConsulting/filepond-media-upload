e5c74ab8eb3b68113b952e752be8164af61faf12
Noel Sharpe <noels@radnetwork.co.uk>
2022-01-08 15:56:11 +0000

    Move filepond to FilepondField component.

--- src/App.js
+++ src/App.js
@@ -6,17 +6,7 @@ import api from './api';
 import DemoForm from './componenets/forms/DemoForm';
 
 function App() {
-  const API_ENDPOINT = '/api/media/getUrlForFileUpload';
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const url = `https://s3-media-upload-globalimpact-world.s3-accelerate.amazonaws.com/uat/39f87176-9391-40d3-bfbd-4c72d0541c27`
-  const initialFiles = [{
-    source: url,
-    options: {
-      type: "local"
-    }
-  }];
-
-
   useEffect( () => {
     (async () => {
       const resp = await api.get('/api/users/me');
@@ -31,7 +21,6 @@ function App() {
     <div className="App">
       <Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
       {isLoggedIn && <>
-          <S3FilePond imageCropAspectRatio={0} presignedUrlEndpoint={API_ENDPOINT} initialFiles={initialFiles}/>
           <DemoForm/>
       </>}
     </div>
--- src/componenets/formik/FilepondField.tsx
+++ src/componenets/formik/FilepondField.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+import S3FilePond from "../media/S3FilePond";
 
 interface OwnProps {
 
 }
 const FilepondField: React.FC<OwnProps> = (props) => {
+  const API_ENDPOINT = '/api/media/getUrlForFileUpload';
+  const url = `https://s3-media-upload-globalimpact-world.s3-accelerate.amazonaws.com/uat/39f87176-9391-40d3-bfbd-4c72d0541c27`
+  const initialFiles = [{
+    source: url,
+    options: {
+      type: "local"
+    }
+  }];
+
   return (
-  <div></div>
+  <S3FilePond imageCropAspectRatio={0} presignedUrlEndpoint={API_ENDPOINT} initialFiles={initialFiles}/>
   );
 }
 
